4ad55b2822ddb59ee70d4006319971bfea8af360
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-06 20:38:58 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_MXFdZTRyLF",
+  "homePage" : "Main",
+  "name" : "Auto_MXFdZTRyLF",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
